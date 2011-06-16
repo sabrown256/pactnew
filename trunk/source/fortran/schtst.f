f9cf19ce8541c8041a1ae1fa95e4628154f5e694
sabrown256 <sabrown256@f80487b2-16bd-11df-9ac1-3b3d05a0ff07>
2011-06-16 03:08:53 +0000

    -m ' < sabrown |X| D11_06_15_20_02_14 | Remote gerard > Get Fortran tests to compile'

--- trunk/source/fortran/schtst.f
+++ trunk/source/fortran/schtst.f
@@ -1,20 +1,19 @@
-*
-* Fortran routine to test the Fortran application interface to
-* the PACT score hash routines
-*
-* Source Version: 3.0
-* Software Release #: LLNL-CODE-422942
-*
-* include "cpyright.h"
-*
+!
+! SCHTST.F - test the SCORE hash function Fortran API
+!
+! Source Version: 3.0
+! Software Release #: LLNL-CODE-422942
+!
+! include "cpyright.h"
+!
 
+!--------------------------------------------------------------------------
+!--------------------------------------------------------------------------
 
-c --------------------------------------------------------------------------
-c --------------------------------------------------------------------------
-
-c SCHTST - main test routine
+! SCHTST - main test routine
 
       program schtst
+      use pact_fortran
       implicit none 
 
       integer tab
@@ -24,53 +23,43 @@ c SCHTST - main test routine
       integer LAST
       data  LAST /0/
 
-c ... SCORE functions
-      integer schlkp, schins, scmkht, scmake
-
       if (scmake(ipx, 3, 8) .eq. 0) then
          stop 1
       endif
 
-      x(1) =  8.0d0
-      x(2) =  9.0d0
-      x(3) = 10.0d0
+      x(1) =  8.0
+      x(2) =  9.0
+      x(3) = 10.0
 
       tab = scmkht(127, 0)
 
-      if (schins(1, 'a', 3, 3, 'int', 0, tab) .eq. 0) then
+      if (schins(1, 'a', 3, 3, 'int', 0, tab) .eq. 0) &
          stop 1
-      endif
 
-      if (schins(1, 'b', 3.14159d0, 6, 'double', 0, tab) .eq. 0) then
+      if (schins(1, 'b', 3.14159d0, 6, 'double', 0, tab) .eq. 0) &
          stop 1
-      endif
 
-      if (schins(1, 'c', x(1), 6, 'double', 0, tab) .eq. 0) then
+      if (schins(1, 'c', x(1), 6, 'double', 0, tab) .eq. 0) &
          stop 1
-      endif
 
-      if (schins(1, 'd', ipx, 8, 'double *', 0, tab) .eq. 0) then
+      if (schins(1, 'd', ipx, 8, 'double *', 0, tab) .eq. 0) &
          stop 1
-      endif
 
       y = 0.0d0
-      if (schlkp(y, 1, 'b', tab) .eq. 0) then
+      if (schlkp(y, 1, 'b', tab) .eq. 0) &
          stop 1
-      endif
 
       write(6, 100) y
 
       y = 0.0d0
-      if (schlkp(y, 1, 'c', tab) .eq. 0) then
+      if (schlkp(y, 1, 'c', tab) .eq. 0) &
          stop 1
-      endif
 
       write(6, 200) x(1), y
 
       ipz = 0
-      if (schlkp(ipz, 1, 'd', tab) .eq. 0) then
+      if (schlkp(ipz, 1, 'd', tab) .eq. 0) &
          stop 1
-      endif
 
       write(6, 200) x(2), z(2)
 
@@ -80,5 +69,5 @@ c ... SCORE functions
       stop 0
       end
 
-c --------------------------------------------------------------------------
-c --------------------------------------------------------------------------
+!--------------------------------------------------------------------------
+!--------------------------------------------------------------------------
