650a86b670db5ea010ca975c497511dc6ee3c818
sabrown256 <sabrown256@f80487b2-16bd-11df-9ac1-3b3d05a0ff07>
2011-06-22 02:22:58 +0000

    -m ' < sabrown |P| D11_06_21_19_21_01 | Remote gerard > Get in blang ARG improvements'

--- trunk/source/psh/tests/ref/gs-bl6.c
+++ trunk/source/psh/tests/ref/gs-bl6.c
@@ -31,48 +31,34 @@ static object *_SXI_fa1(SS_psides *si, object *argl)
 /*--------------------------------------------------------------------------*/
 /*--------------------------------------------------------------------------*/
 
-/* WRAP |int fa2(int a ARG([1,2,3],in), int b ARG([1,2,3],io), int c ARG([1,2,3],out))| */
-
-static object *_SXI_fa2(SS_psides *si, object *argl)
-   {int _la;
-    int _lb;
-    int _lc;
-    int _rv;
-    object *_lo;
-
-    _la        = NULL;
-    _lb        = NULL;
-    _lc        = NULL;
-    SS_args(si, argl,
-            SC_INT_I, &_la,
-            SC_INT_I, &_lb,
-            SC_INT_I, &_lc,
-            0);
-
-    _rv = fa2(_la, _lb, _lc);
-    _lo = SS_mk_integer(si, _rv);
-
-    return(_lo);}
-
-/*--------------------------------------------------------------------------*/
-/*--------------------------------------------------------------------------*/
-
 /* WRAP |int fa3(int *a ARG([1,2,3],in), int *b ARG([1,2,3],io), int *c ARG([1,2,3],out))| */
 
 static object *_SXI_fa3(SS_psides *si, object *argl)
-   {int *_la;
-    int *_lb;
-    int *_lc;
+   {int _la[3];
+    int _lb[3];
+    int _lc[3];
     int _rv;
     object *_lo;
 
-    _la        = NULL;
-    _lb        = NULL;
-    _lc        = NULL;
+    _la[0] = 1;
+    _la[1] = 2;
+    _la[2] = 3;
+    _lb[0] = 1;
+    _lb[1] = 2;
+    _lb[2] = 3;
+    _lc[0] = 1;
+    _lc[1] = 2;
+    _lc[2] = 3;
     SS_args(si, argl,
-            SC_INT_P_I, &_la,
-            SC_INT_P_I, &_lb,
-            SC_INT_P_I, &_lc,
+            SC_INT_I, &_la[0],
+            SC_INT_I, &_la[1],
+            SC_INT_I, &_la[2],
+            SC_INT_I, &_lb[0],
+            SC_INT_I, &_lb[1],
+            SC_INT_I, &_lb[2],
+            SC_INT_I, &_lc[0],
+            SC_INT_I, &_lc[1],
+            SC_INT_I, &_lc[2],
             0);
 
     _rv = fa3(_la, _lb, _lc);
@@ -116,11 +102,6 @@ void SX_install_bl6_bindings(SS_psides *si)
                "Procedure: fa1s\n     Usage: (fa1s a b c)",
                SS_nargs,
                _SXI_fa1, SS_PR_PROC);
-
-    SS_install(si, "fa2s",
-               "Procedure: fa2s\n     Usage: (fa2s a b c)",
-               SS_nargs,
-               _SXI_fa2, SS_PR_PROC);
 
     SS_install(si, "fa3s",
                "Procedure: fa3s\n     Usage: (fa3s a b c)",
