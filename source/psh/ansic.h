a850a9cc1dd5482e46c20f626941a672cc06e81c
sabrown256 <sabrown256@f80487b2-16bd-11df-9ac1-3b3d05a0ff07>
2012-06-22 03:22:28 +0000

    ' < sabrown |X| D12_06_21_20_13_34 | Remote gerard > Use psh/ansic.h in score/scstd.h to supply ANSI headers'

---
+++ source/psh/ansic.h
@@ -0,0 +1,66 @@
+/*
+ * ANSIC.H - header to provide ANSI C environment
+ *
+ * Source Version: 3.0
+ * Software Release #: LLNL-CODE-422942
+ *
+ * include "cpyright.h"
+ *
+ */
+
+#ifndef PCK_ANSIC
+#define PCK_ANSIC
+
+#ifndef NO_STD_INCLUDES
+# include <stdlib.h>
+# include <string.h>
+# include <stdio.h>
+# include <stddef.h>
+# include <stdarg.h>
+# include <stdint.h>
+# include <limits.h>
+# include <math.h>
+# include <stdbool.h>
+# include <ctype.h>
+# include <wctype.h>
+# include <float.h>
+
+# ifndef __cplusplus
+#  ifdef HAVE_ANSI_C9X_COMPLEX
+#   include <complex.h>
+#  endif
+# endif
+
+# ifndef S_SPLINT_S
+#  include <signal.h>
+# endif
+
+# include <time.h>
+# include <errno.h>
+# include <setjmp.h>
+# include <assert.h>
+
+#endif
+
+#undef TRUE
+#undef FALSE
+
+#define TRUE       1
+#define FALSE      0
+
+#ifndef __cplusplus
+
+# undef max
+# ifndef max
+#  define max(a, b) ((a) > (b) ? (a) : (b))
+# endif
+
+# undef min
+# ifndef min
+#  define min(a, b) ((a) < (b) ? (a) : (b))
+# endif
+
+#endif
+
+#endif
+
