43be819e84c31f0c142d65c1b6d6b666c1620324
sabrown256 <sabrown256@f80487b2-16bd-11df-9ac1-3b3d05a0ff07>
2012-08-11 14:23:22 +0000

    ' < sabrown |P| D12_08_11_07_23_17 | Remote gerard > Working strong functions'

--- trunk/source/psh/gexec.c
+++ trunk/source/psh/gexec.c
@@ -16,7 +16,7 @@
 /* DO_TEST - do the I/O work for the test function */
 
 static int do_test(FILE **fio, char *tag)
-   {int i, rv;
+   {int i, rv, ev;
     char t[MAXLINE];
     char *p;
     static int count = 0;
@@ -33,66 +33,21 @@ printf("test> (%d,%d,%d)  stdin=%d  stdout=%d  stderr=%d\n",
         {if (feof(fio[0]) == TRUE)
 	    rv = 1;
 	 else
-	    {p = fgets(t, MAXLINE, fio[0]);
+	    {p  = fgets(t, MAXLINE, fio[0]);
+             ev = errno;
 	     if (p != NULL)
 	        {count = 0;
 		 fprintf(fio[1], "%s> out: %s", tag, p);}
+	     else if (feof(fio[0]) == TRUE)
+	        rv = 1;
+	     else if (ev == EBADF)
+	        rv = 2;
 	     else
 	        {fprintf(fio[1], "%s> gen: %d\n", tag, ++count);
 		 rv = (count > 6);};
 
-	    sched_yield();};};
-
-    return(rv);}
-
-/*--------------------------------------------------------------------------*/
-/*--------------------------------------------------------------------------*/
-
-/* DO_FNC - abstract function/procedure as process for GEXEC
- *        - mode -r means stdin to fnc
- *        -      -w means fnc to stdout
- *        -      -a means stdin to fnc to stdout
- *        - this is a weak form of implementation because a separate process
- *        - is launched to wrap the function - context for F is lost
- *        - a strong form will involve changing run_pgrp, reconnect_pgrp,
- *        - and parse_pgrp to poll the input descriptors for the function
- *        - also to call the function for output
- */
-
-static int do_fnc(char *db, int c, char **v, PFPCAL (*map)(char *x))
-   {int i, rv, err_exist;
-    io_mode md;
-    char *fn;
-    FILE *fio[2];
-    PFPCAL f;
-
-    rv = -1;
-    md = IO_MODE_NONE;
-
-    err_exist = FALSE;
-
-    for (i = 0; i < 2; i++)
-        {if (strcmp(v[i], "-r") == 0)
-	    md = IO_MODE_RO;
-	 else if (strcmp(v[i], "-w") == 0)
-	    md = err_exist ? IO_MODE_WO : IO_MODE_WD;
-	 else if ((strcmp(v[i], "-rw") == 0) || (strcmp(v[i], "-a") == 0))
-	    md = IO_MODE_APPEND;
-	 else
-	    fn = v[i];};
-
-    if ((fn != NULL) && (md != IO_MODE_NONE))
-       {c -= 2;
-	v += 2;
-
-/* real application would map name FN to actual function F to call */
-	if (map != NULL)
-	   {f = map(fn);
-	    if (f != NULL)
-	       {fio[0] = stdin;
-		fio[1] = stdout;
-
-		rv = f(db, md, fio, c, v);};};};
+	     nsleep(10);};};
+/*	     sched_yield(); */
 
     return(rv);}
 
@@ -255,6 +210,8 @@ static int str_file(char *db, io_mode md, FILE **fio, int c, char **v)
 			      ev = errno;
 			      if (p != NULL)
 				 fputs(p, fp);
+			      else if (feof(fio[0]) == TRUE)
+				 rv = 1;
 			      else if (ev == EBADF)
 			         rv = 2;};};
 
@@ -264,8 +221,11 @@ static int str_file(char *db, io_mode md, FILE **fio, int c, char **v)
 	        default :
 		     break;};
 
+/*
 	    if (rv == 1)
-	       fclose(fp);};};
+	       fclose(fp);
+*/
+	   };};
 
     return(rv);}
 
@@ -308,194 +268,6 @@ static PFPCAL maps(char *s)
 /*--------------------------------------------------------------------------*/
 /*--------------------------------------------------------------------------*/
 
-/* EXE_VAR - function to access variables */
-
-static int exe_var(char *db, io_mode md, FILE **fio, int c, char **v)
-   {int i, n, nc, ns, rv;
-    char t[MAXLINE];
-    char *p, *vr, *vl, **sa;
-
-    rv = 0;
-    vr = v[0];
-
-    if ((vr != NULL) && (md != IO_MODE_NONE))
-       {sa = NULL;
-
-        vl = getenv(vr);
-	if (vl != NULL)
-	   sa = lst_add(sa, vl);
-
-	switch (md)
-
-/* variable to stdout NC times */
-	   {case IO_MODE_RO :
-	         nc = 1;
-		 ns = lst_length(sa);
-		 for (n = 0; n < nc; n++)
-		     {for (i = 0; i < ns; i++)
-			  printf("%s ", sa[i]);
-		      printf("\n");};
-		 break;
-
-/* stdin to variable */
-	    case IO_MODE_WO :
-	    case IO_MODE_WD :
-	         while (feof(fio[0]) == FALSE)
-		    {p = fgets(t, MAXLINE, fio[0]);
-		     if (p != NULL)
-		        {LAST_CHAR(t) = '\0';
-			 sa = lst_add(sa, t);
-		       sched_yield();};};
-
-		 vl = concatenate(t, MAXLINE, sa, " ");
-
-		 if (IS_NULL(db) == FALSE)
-		    dbset(NULL, vr, vl);
-		 else
-		    printf("setenv %s \"%s\" ; ", vr, vl);
-
-		 break;
-
-/* stdin append to variable */
-	    case IO_MODE_APPEND :
-	         while (feof(fio[0]) == FALSE)
-		    {p = fgets(t, MAXLINE, fio[0]);
-		     if (p != NULL)
-		        {LAST_CHAR(t) = '\0';
-			 sa = lst_add(sa, t);
-			 vl = concatenate(t, MAXLINE, sa, " ");
-			 fprintf(fio[1], "%s\n", vl);};
-		       sched_yield();};
-
-		 if (IS_NULL(db) == FALSE)
-		    dbset(NULL, vr, vl);
-		 else
-		    printf("setenv %s \"%s\" ; ", vr, vl);
-
-		 break;
-
-	    default :
-		 break;};
-
-	free_strings(sa);};
-
-    return(rv);}
-
-/*--------------------------------------------------------------------------*/
-/*--------------------------------------------------------------------------*/
-
-/* EXE_FILE - function to access files */
-
-static int exe_file(char *db, io_mode md, FILE **fio, int c, char **v)
-   {int rv;
-    char t[MAXLINE];
-    char *p, *fn;
-    FILE *fp;
-
-    rv = 0;
-    fn = v[0];
-
-    if ((fn != NULL) && (md != IO_MODE_NONE))
-       {switch (md)
-
-/* file to stdout */
-	   {case IO_MODE_RO :
-	         fp = fopen(fn, "r");
-		 if (fp == NULL)
-		    fprintf(stderr, "Cannot open '%s' for reading\n", fn);
-		 else
-		    {while (feof(fp) == FALSE)
-		        {p = fgets(t, MAXLINE, fp);
-			 if (p != NULL)
-			    fputs(p, fio[1]);
-			 sched_yield();};
-
-		     fclose(fp);};
-		 break;
-
-	    case IO_MODE_WO :
-		 if (file_exists(fn) == TRUE)
-		    return(-1);
-
-/* stdin to file */
-	    case IO_MODE_WD :
-		 fp = fopen(fn, "w");
-		 if (fp == NULL)
-		    fprintf(stderr, "Cannot open '%s' for writing\n", fn);
-		 else
-		    {while (feof(fio[0]) == FALSE)
-		        {p = fgets(t, MAXLINE, fio[0]);
-			 if (p != NULL)
-			    fputs(p, fp);
-			 sched_yield();};
-
-		     fclose(fp);};
-		 break;
-
-/* stdin append to file */
-	    case IO_MODE_APPEND :
-		 fp = fopen(fn, "a");
-		 if (fp == NULL)
-		    fprintf(stderr, "Cannot open '%s' for append\n", fn);
-		 else
-		    {while (feof(fio[0]) == FALSE)
-		        {p = fgets(t, MAXLINE, fio[0]);
-			 if (p != NULL)
-			    fputs(p, fp);
-			 sched_yield();};
-
-		     fclose(fp);};
-		 break;
-
-	    default :
-		 break;};};
-
-    return(rv);}
-
-/*--------------------------------------------------------------------------*/
-/*--------------------------------------------------------------------------*/
-
-/* EXE_TEST - function to test */
-
-static int exe_test(char *db, io_mode md, FILE **fio, int c, char **v)
-   {int rv;
-    char *tag;
-    static int count = 0;
-
-    rv  = 0;
-    tag = v[0];
-
-    switch (md)
-       {case IO_MODE_RO :
-	     fprintf(fio[1], "%s> gen: %d\n", tag, ++count);
-	     break;
-        default :
-	     rv = do_test(fio, tag);
-	     rv = 0;
-	     break;};
-
-    return(rv);}
-
-/*--------------------------------------------------------------------------*/
-/*--------------------------------------------------------------------------*/
-
-/* MAPF - map function name to procedure for execution */
-
-static PFPCAL mapf(char *s)
-   {PFPCAL f;
-
-    if (strcmp(s, "var") == 0)
-       f = exe_var;
-    else if (strcmp(s, "file") == 0)
-       f = exe_file;
-    else if (strcmp(s, "test") == 0)
-       f = exe_test;
-
-    return(f);}
-
-/*--------------------------------------------------------------------------*/
-/*--------------------------------------------------------------------------*/
-
 /* HELP - print help message */
 
 static int help(void)
@@ -514,8 +286,8 @@ int main(int c, char **v, char **env)
    {int i, rv;
     char *db, *s;
 
-    db = cgetenv(TRUE, "PERDB_PATH");
-    s  = cgetenv(TRUE, "GEXEC_DEBUG_LEVEL");
+    db = getenv("PERDB_PATH");
+    s  = getenv("GEXEC_DEBUG_LEVEL");
 
     dbg_level = (s == NULL) ? 0 : atol(s);
 
@@ -528,22 +300,9 @@ int main(int c, char **v, char **env)
             {dbg_level = atol(v[++i]);
              csetenv("GEXEC_DEBUG_LEVEL", "%d", dbg_level);}
 
-	 else if (strcmp(v[i], "-s") == 0)
-            strong_functions = TRUE;
-
-	 else if (strcmp(v[i], "-w") == 0)
-            strong_functions = FALSE;
-
-	 else if (strong_functions == TRUE)
-	    {rv = gexec(db, c-i, v+i, env, maps);
-	     break;}
-
-	 else if (strcmp(v[i], "-p") == 0)
-            {rv = do_fnc(db, c-i-1, v+i+1, mapf);
-	     break;}
 	 else
-            {rv = gexec(db, c-i, v+i, env, NULL);
-	     break;};};
+	    {rv = gexec(db, c-i, v+i, env, maps);
+	     break;}};
 
     return(rv);}
 
