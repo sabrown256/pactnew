72adabcf937d1420ed0683f5aa0004c04da5698f
sabrown256 <sabrown256@f80487b2-16bd-11df-9ac1-3b3d05a0ff07>
2013-03-01 21:54:37 +0000

    ' < sabrown |P| D13_03_01_13_54_30 | Remote gerard > Make hello-omp.c more useful'

--- source/psh/hello-omp.c
+++ source/psh/hello-omp.c
@@ -1,13 +1,15 @@
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <omp.h>
 
 int main(int c, char **v)
    {int nt;
 
-    nt = 2;
+    if (getenv("OMP_NUM_THREADS") == NULL)
+       {nt = 2;
 
-    omp_set_num_threads(nt);
+	omp_set_num_threads(nt);};
 
 #pragma omp parallel
     printf("Hello thread %d\n", omp_get_thread_num());
