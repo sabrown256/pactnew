9ca6dd47b271e08b9a09885f97b2190ad13b5a8e
sabrown256 <sabrown256@f80487b2-16bd-11df-9ac1-3b3d05a0ff07>
2014-10-30 02:40:41 +0000

    ' < sabrown |X| D14_10_29_19_33_36 | Remote fiona > Rearrange python binding sources'

--- source/python/pdbdata.c
+++ source/python/pdbdata.c
@@ -1,25 +1,13 @@
-/*--------------------------------------------------------------------------*/
-
-/* This is generated code.
- * Any edits must be made between the splicer.begin and splicer.end blocks.
- * All other edits will be lost.
- * Once a block is edited remove the 'UNMODIFIED' on the splicer.begin comment
- * to allow the block to be preserved when it is regenerated.
- */
 /*
- * Source Version: 9.0
- * Software Release: LLNL-CODE-422942
+ * PDBDATA.C
  *
  * include cpyright.h
  */
-/*--------------------------------------------------------------------------*/
+
 #include "pdbmodule.h"
 
-/* DO-NOT-DELETE splicer.begin(pdb.pdbdata.C_definition) */
-
-static PyObject *indexerr = NULL;
-
-/* DO-NOT-DELETE splicer.end(pdb.pdbdata.C_definition) */
+static PyObject
+ *indexerr = NULL;
 
 /*--------------------------------------------------------------------------*/
 /*--------------------------------------------------------------------------*/
