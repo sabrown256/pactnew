985785e5503f5dc8bfc82d6a117fa87adb20171c
sabrown256 <sabrown256@f80487b2-16bd-11df-9ac1-3b3d05a0ff07>
2015-01-25 18:55:15 +0000

    ' < sabrown |X| D15_01_25_10_40_33 | Remote fiona > Use acc for everything'

--- source/pdb/tests/chk++.cc
+++ source/pdb/tests/chk++.cc
@@ -10,7 +10,8 @@
 //--------------------------------------------------------------------------
 
 int main (int c, char **v)
-   {int rv;
+   {int rv, rd;
+    defstr *dp;
     PDBfile *fp;
 
 // test compile these:
@@ -19,7 +20,21 @@ int main (int c, char **v)
 
     fp = PD_open("c++.dat", "w");
     rv = (fp != NULL);
+
     PD_write(fp, "v1", "int", &rv);
+    PD_read(fp, "v1", &rd);
+    PD_append(fp, "v1", &rd);
+
+    dp = PD_defstr(fp, "foo", "int a", NULL);
+    dp = PD_inquire_type(fp, "foo");
+    rv = (dp != NULL);
+
+    PD_mkdir(fp, "/d1");
+    PD_cd(fp, "/d1");
+    rd = PD_isdir(fp, "/d1");
+    PD_ln(fp, "/d1", "/d2");
+    PD_ls(fp, "/d1", NULL, NULL);
+
     PD_close(fp);
 
     return(rv);}
